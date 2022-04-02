a7cd3c2093ed342d66343ed88538f3bd96baeacd
gergana2120000 <gergana.ivanova212000@gmail.com>
2022-04-02 12:54:05 +0300

    create a project

---
+++ real_estate_project/src/main/java/bg/tuvarna/sit/real_estate_proj/HelloController.java
@@ -0,0 +1,14 @@
+package bg.tuvarna.sit.real_estate_proj;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
